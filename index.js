9dc22f26d850b2876321dd5008e2e11da94fdae1
chriselian8 <chriselian8@gmail.com>
2022-07-04 19:18:43 -0400

    ready to deploy

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 5000
 //process.env.PORT
 //process.env.NODE_ENV => production or undefined
 
-
 //middleware
 app.use(cors())
 app.use(express.json())
+// app.use(express.static('client/build'))
 
 if (process.env.NODE_ENV === 'production') {
    //server static content
@@ -82,6 +82,10 @@ app.delete('/movies/:id', async(req, res) => {
       console.log(err.message)
    }
 })
+
+// app.get('*', (req, res) => {
+//    res.sendFile(path.join(__dirname, 'client/build/index.html'))
+// })
 
 app.listen(PORT, () => {
    console.log(`Server has started on port ${PORT}`)
